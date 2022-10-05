1313f95f8c2a0f3d9ca498d2d90d4e159536e942
Danil Kyrychenko <danilkyrychenko@MacBook-Air-Danil.local>
2022-10-05 16:39:22 +0300

    first commit

---
+++ my1.py
@@ -0,0 +1,71 @@
+from email import message
+
+import telebot
+from telebot import types
+from telebot.types import InlineKeyboardMarkup
+
+name = ' '
+surname = ' '
+age = 0
+
+bot = telebot.TeleBot("5580320258:AAEYAMH3Gg_XZApHEi4hOgzk7uNqmszUDX8")
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Напишите мне /reg или просто нажмите сюда -> /reg")
+
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    if message.text == 'Привет':
+        bot.reply_to(message, 'Привет создатель бота')
+    elif message.text == 'hi':
+        bot.reply_to(message, 'hi again !')
+    elif message.text == '/reg':
+        bot.send_message(message.from_user.id, 'Привет! давай познакомимся! Как тебя зовут ? ')
+    bot.register_next_step_handler(message, reg_name)
+
+    # bot.reply_to(message, message.text)
+
+
+def reg_name(message):
+    global name
+    name = message.text
+    bot.send_message(message.from_user.id, 'Какая твоя фамилия ?')
+    bot.register_next_step_handler(message, reg_surname)
+
+
+def reg_surname(message):
+    global surname
+    surname = message.text
+    bot.send_message(message.from_user.id, 'Сколько вам лет ?')
+    bot.register_next_step_handler(message, reg_age)
+
+
+def reg_age(message):
+    global age
+    while age == 0:
+        try:
+            age = int(message.text)
+        except Exception:
+            bot.send_message(message.from_user.id, 'Вводите цифрами!')
+
+    keyboard = types.InlineKeyboardMarkup()
+    key_yes = types.InlineKeyboardButton(text='да', callback_data='yes')
+    keyboard.add(key_yes)
+    key_no = types.InlineKeyboardButton(text='нет', callback_data='no')
+    keyboard.add(key_no)
+    question = "Тебе " + str(age) + ' лет ? И тебя зовут: ' + name + ' ' + surname + " ? "
+    bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == "yes":
+        bot.send_message(call.message.chat.id, "Приятно познакомиться! Теперь это идет в Базу данных. Хорошего дня! :)")
+    elif call.data == "no":
+        bot.send_message(call.message.chat.id, "Попробуем еще раз :)")
+        bot.send_message(call.message.chat.id, 'Привет! давай познакомимся! Как тебя зовут ?')
+        bot.register_next_step_handler(call.message, reg_name)
+
+bot.polling()
